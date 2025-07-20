66b4e274ec8da92d97073f3a48f1b700e1c744cd
Mohamed Adan <adan@192.168.0.4>
2025-07-21 01:57:58 +0800

    ac

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the API!' });
+});
+
+app.get('/ping', (req, res) => {
+    res.json({ message: 'pong' });
+});
+
+app.listen(port, () => {
+    console.log(`API server running at http://localhost:${port}`);
+});
